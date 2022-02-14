c7182d0d9356ad15d71c355a3256c8bc2589b005
RomanChursin <romanchursin@me.com>
2022-02-13 21:26:58 -0800

    Load to Git

--- conftest.py
+++ conftest.py
@@ -3,12 +3,23 @@ Root file for all the high priority fixtures.
 In this file fixtures can use across the architecture.
 Avoid placing unnecessary fixture in here.
 """
+import logging
 
+import pytest
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.service import Service
 
 
-class Browser:
+@pytest.fixture(scope="class")
+def setup(request):
+    logging.info("initiating chrome driver")
     service = Service(executable_path=GeckoDriverManager().install())
-    browser = webdriver.Firefox(service=service)
+    driver = webdriver.Firefox(service=service)
+    driver.get("http://kajabi.com")
+    request.cls.driver = driver
+
+    yield driver
+    driver.close()
+
+
