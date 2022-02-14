06fa67956538444fc34252081d18dcdf33155a1f
RomanChursin <romanchursin@me.com>
2022-02-13 16:08:08 -0800

    New

--- Conftest.py
+++ Conftest.py
@@ -3,3 +3,12 @@ Root file for all the high priority fixtures.
 In this file fixtures can use across the architecture.
 Avoid placing unnecessary fixture in here.
 """
+
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
+
+
+class Browser:
+    service = Service(executable_path=GeckoDriverManager().install())
+    browser = webdriver.Firefox(service=service)
