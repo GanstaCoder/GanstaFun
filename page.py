3fee34341bc9f896955b6f70a2254b9c74a9d1a1
RomanChursin <romanchursin@me.com>
2022-02-13 22:47:10 -0800

    Changes to Framework

--- page.py
+++
@@ -1,8 +0,0 @@
-class Training:
-    def __init__(self, driver):
-        self.driver = driver
-
-    def click_button(self):
-        button = self.driver.find_element_by_css_selector("href$='/login'")
-        button.click()
-        return Training
