439f2d14eb3b3a9f3fdd294c7aaff8c5c1000d2e
RomanChursin <romanchursin@me.com>
2022-02-18 17:53:49 -0800

    WIP

--- tests/test_exmple.py
+++ tests/test_exmple.py
@@ -1,2 +1,11 @@
 #TODO
 # On the way...
+import pytest
+
+from page_object_factory.trial_page import TrialPage
+
+@pytest.mark.usefixtures("setup")
+class TrialPage:
+# Trial Page
+    def test_test_page:
+        trial_page =
